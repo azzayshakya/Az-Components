0820d81910f315893db94015d152a3a49de49ca2
Ajay <ajayshakya7376@gmail.com>
2025-12-21 21:16:55 +0530

    fix :  header and sidebar fix

--- src/admin/components/AdminHeaderComponent.jsx
+++ src/admin/components/AdminHeaderComponent.jsx
@@ -1,44 +1,51 @@
-import { Breadcrumb,  Layout,  } from 'antd';
-import {
-  MenuFoldOutlined,
-  MenuUnfoldOutlined,
-
-} from '@ant-design/icons';
+import { Breadcrumb, Layout } from "antd";
+import { MenuFoldOutlined, MenuUnfoldOutlined } from "@ant-design/icons";
 const { Header } = Layout;
 
-export default function AdminHeaderComponent({ colorBgContainer,setCollapsed,collapsed,breadcrumbItems,}) {
+export default function AdminHeaderComponent({
+  colorBgContainer,
+  setCollapsed,
+  collapsed,
+  breadcrumbItems,
+}) {
   return (
-    <div><Header
+    <div>
+      <Header
+        style={{
+          padding: "0 24px",
+          background: colorBgContainer,
+          display: "flex",
+          alignItems: "center",
+          gap: 16,
+          borderBottom: "2px solid #f0f0f0",
+          position: "sticky",
+          top: 0,
+          zIndex: 1,
+        }}
+      >
+        <div
           style={{
-            padding: '0 24px',
-            background: colorBgContainer,
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'space-between',
-            borderBottom: '1px solid #f0f0f0',
-            position: 'sticky',
-            top: 0,
-            zIndex: 1,
+            fontSize: 18,
+            cursor: "pointer",
+            display: "flex",
+            alignItems: "center",
+          }}
+          onClick={() => setCollapsed(!collapsed)}
+        >
+          {collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
+        </div>
+
+        <div
+          style={{
+            display: "flex",
+            alignItems: "center",
           }}
         >
-          <div
-            style={{
-              fontSize: '18px',
-              cursor: 'pointer',
-            }}
-            onClick={() => setCollapsed(!collapsed)}
-          >
-            {collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
-          </div>
-          <>
           {breadcrumbItems.length > 0 && (
-            <Breadcrumb
-              style={{ margin: '0 0 16px 0' }}
-              items={breadcrumbItems}
-            />
-          )}</>
-
-         
-        </Header></div>
-  )
+            <Breadcrumb items={breadcrumbItems} style={{ margin: 0 }} />
+          )}
+        </div>
+      </Header>
+    </div>
+  );
 }
