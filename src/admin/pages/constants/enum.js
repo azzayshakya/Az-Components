75a5fd5723ea11f5f5c1393ced720d0a4b190fd5
Ajay <ajayshakya7376@gmail.com>
2025-12-25 21:09:42 +0530

    feat :  enums added

---
+++ src/admin/pages/constants/enum.js
@@ -0,0 +1,25 @@
+export const GENDER = [
+  { label: 'Male', value: 'male' },
+  { label: 'Female', value: 'female' },
+  { label: 'Other', value: 'other' },
+]
+
+export const DEPARTMENT = [
+  { label: 'Civil Engineering', value: 'civil' },
+  { label: 'Electrical Engineering', value: 'electrical' },
+  { label: 'Mechanical Engineering', value: 'mechanical' },
+  { label: 'Fire Fighting', value: 'firefighting' },
+  { label: 'Site Engineer', value: 'site' },
+]
+
+export const WORK_STATUS = [
+  { label: 'Currently Working', value: 'working' },
+  { label: 'Resigned', value: 'resigned' },
+  { label: 'Terminated', value: 'terminated' },
+]
+
+export const WORK_LOCATION = [
+  { label: 'Work From Office', value: 'wfo' },
+  { label: 'Work From Home', value: 'wfh' },
+  { label: 'Site Work', value: 'site' },
+]
