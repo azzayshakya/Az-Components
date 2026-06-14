73e6400c14f718eed0a1e605d8533ab9f1bff025
Ajay <ajayshakya7376@gmail.com>
2026-06-14 18:48:13 +0530

    Removing Azc from az Components

--- azc/server/routes/messageRoutes.js
+++
@@ -1,7 +0,0 @@
-const router = require("express").Router();
-const { getMessages, markRead } = require("../controllers/messageController");
-
-router.get("/:userId/:otherId", getMessages); //get conversation bw two users
-router.patch("/read", markRead);
-
-module.exports = router;
--- azc/server/routes/userRoutes.js
+++
@@ -1,7 +0,0 @@
-const router = require("express").Router();
-const { searchUsers, getContacts } = require("../controllers/userController");
-
-router.get("/users/search", searchUsers);
-router.get("/contacts/:userId", getContacts);
-
-module.exports = router;
